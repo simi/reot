07bc7b82c8468c75aba1e55389731c5636bd2d15
Josef Šimánek <retro@ballgag.cz>
2012-03-12 08:22:01 +0100

    remove correct file in test

--- test/reot_test.rb
+++ test/reot_test.rb
@@ -2,7 +2,7 @@ require 'helper'
 
 class TestReot < MiniTest::Unit::TestCase
   def setup
-    File.delete('test/fonts/Metalista.ttf.eot') if File.exists?("test/fonts/Metalista.ttf.eot")
+    File.delete('test/fonts/Metalista.eot') if File.exists?("test/fonts/Metalista.eot")
   end
 
   def test_reot_converts_ttf_to_eot
